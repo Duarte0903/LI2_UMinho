3e29eb6c5900b97b91074263c07c2e7d5877eb72
a100550 <a100550@alunos.uminho.pt>
2022-03-26 21:19:15 +0000

    .......

--- main.c
+++ main.c
@@ -1,68 +1,14 @@
-#include "funcoes.c"
+#include "operacoes.c"
 #include <stdio.h>
 #include <string.h>
 
-
-int subtracao (char a[], int len, int j)
-{
-    int i, sub, s[100];
-
-    if (j==0)
-    {
-        sub = char_to_int (a[0]);
-
-        for (i = 1; i < len; i++)
-        {
-            if (a[i] != ' ') 
-            {
-                sub = sub - char_to_int (a[i]);
-            }
-        }
-    }
-
-    else 
-    {
-        for (i = j + 1; i < len; i++)
-        {
-            if (a[i] != ' ')
-            {
-                sub = sub - char_to_int (a[i]);
-            }
-        }
-    }
-
-    return sub;
-}
-
-int soma (char a[], int len, int j)
-{
-    int i, sum = 0;
-
-    if (j==0)
-    {
-        for (i = 0; i < len; i++)
-        {
-            if (a[i] != ' ') sum += char_to_int (a[i]);
-        }
-    }
-
-    else
-    {
-        for (i = j + 1; i < len; i++)
-        {
-            if (a[i] != ' ') sum += char_to_int (a[i]);
-        }
-    }
-
-    return sum;
-}
 
 // j guarda o indice do ultimo operador
 
 int main ()
 {
     char a[100];
-    int b[100], i, j = 0, len, resultado = 0;
+    int i, j = 0, len, resultado = 0;
 
     fgets (a, 100, stdin);
 
@@ -87,10 +33,35 @@ int main ()
         {
             int x;
 
-            x = subtracao (a, i,j);
+            x = subtracao (a,i,j,resultado);
 
-            if (j==0) resultado = x;
-            else resultado = resultado - x;
+            resultado = x;
+
+            j = i;
+
+            i++;
+        }
+
+        if (a[i]=='(')
+        {
+            int x;
+
+            x = decrementar (a,j,resultado);
+
+            resultado = x;
+
+            j = i;
+
+            i++;
+        }
+
+        if (a[i]==')')
+        {
+            int x;
+
+            x = incrementar (a,j,resultado);
+
+            resultado = x;
 
             j = i;
 
@@ -98,7 +69,7 @@ int main ()
         }
     }
 
-    printf ("%d", resultado);
+    printf ("%d\n", resultado);
 
     return 0;
 }
